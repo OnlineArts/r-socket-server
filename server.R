12bcdda93b991cfc433ca6e40ce300630c75765f
Roman Martin <web.github@online-arts.de>
2023-08-03 00:20:30 +0200

    Update server code

--- server.R
+++ server.R
@@ -1,13 +1,17 @@
 server <- function(){
   while(TRUE){
     writeLines("Listening...")
-    con <- socketConnection(host="localhost", port = 6011, blocking=TRUE,
-                            server=TRUE, open="r+")
-    data <- readLines(con, 1)
-    print(data)
-    response <- toupper(data) 
-    writeLines(response, con) 
+    con <- socketConnection(host="localhost", port = 6011, blocking=TRUE, server=TRUE, open="r+", timeout=2678400)
+    data = try(readLines(con, 1))
+
+    if(nchar(data) > 0) {
+      print(data)
+      response <- toupper(data) 
+      writeLines(response, con) 
+    }
+
     close(con)
   }
 }
+
 server()
